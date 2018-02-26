893dbfb72579652b7c0691c30bc0a908130638fa
Adam Wick <awick@uhsure.com>
2018-02-25 16:46:56 -0800

    Fix the name.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! # Simple Crypto: A quaint little crypto library for rust.
+//!
+//! This is the simple_crypto library. Its goal is to provide straightforward
+//! coverage of most of the common cryptographic algorithms used today, along
+//! with simplified interfaces for new users. If you see something that's
+//! unclear, please point it out! We can always use documentation help.
+//!
+//! This main library will eventually provide most of the convenience functions
+//! that a new user should use, along with documentation regarding how and
+//! when they should use it, and examples. For now, it mostly just fowards
+//! off to more detailed modules. Help requested!
+
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
+/// The cryptonum module provides support for large numbers at fixed,
+/// cryptographically-relevant sizes.
+pub mod cryptonum;
 
 #[cfg(test)]
 mod test {
