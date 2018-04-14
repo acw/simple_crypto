4985426e74ed473f44b98db15eb79d04cf620296
Adam Wick <awick@uhsure.com>
2018-04-14 07:16:50 -0700

    Prospective prime support.

--- src/cryptonum/mod.rs
+++ src/cryptonum/mod.rs
@@ -2,6 +2,7 @@
 mod conversions;
 #[macro_use]
 mod complete_arith;
+mod primes;
 mod signed;
 mod unsigned;
 #[cfg(test)]
@@ -9,5 +10,4 @@ mod gold_tests;
 
 pub use self::signed::SCN;
 pub use self::unsigned::UCN;
-
-
+pub use self::primes::*;
